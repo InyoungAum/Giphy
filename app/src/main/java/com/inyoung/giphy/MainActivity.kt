8c0d3b9f3d1853402ab6cd40cb67e272239584e4
Inyoung Aum <cxcv92@naver.com>
2020-02-20 17:29:44 +0900

    color placeHolder와 고정 image크기를 통한 자연스러운 로딩

--- app/src/main/java/com/inyoung/giphy/MainActivity.kt
+++ app/src/main/java/com/inyoung/giphy/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.inyoung.giphy
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.inyoung.giphy.Constants.API_KEY
-import com.inyoung.giphy.model.GifImage
 import com.inyoung.giphy.model.SearchResponse
 import com.inyoung.giphy.network.ApiManager
 import com.inyoung.giphy.view.SearchImageAdapter
@@ -15,8 +12,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.util.DisplayMetrics
 
 class MainActivity : Activity() {
+    private val SPAN_COUNT = 2
+    private val IMAGE_LIMIT = 50
     private val recyclerView by lazy { findViewById<RecyclerView>(R.id.recycler_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,14 +24,13 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         setView()
         search("car")
-
     }
 
     private fun search(query: String) {
         ApiManager.getSearchService().checkAppVersion(
             API_KEY,
             query,
-            10,
+            IMAGE_LIMIT,
             0,
             "",
             "ko",
@@ -56,9 +55,16 @@ class MainActivity : Activity() {
 
     private fun setView() {
         recycler_view.apply {
-            adapter = SearchImageAdapter(listOf())
+            val metrics = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(metrics)
+
+            adapter = SearchImageAdapter(
+                listOf(),
+                metrics,
+                SPAN_COUNT
+            )
             layoutManager = StaggeredGridLayoutManager(
-                2, StaggeredGridLayoutManager.VERTICAL
+                SPAN_COUNT, StaggeredGridLayoutManager.VERTICAL
             )
             setHasFixedSize(true)
         }
--- app/src/main/java/com/inyoung/giphy/view/SearchImageAdapter.kt
+++ app/src/main/java/com/inyoung/giphy/view/SearchImageAdapter.kt
@@ -1,5 +1,8 @@
 package com.inyoung.giphy.view
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +13,10 @@ import com.inyoung.giphy.R
 import com.inyoung.giphy.model.GifImage
 
 class SearchImageAdapter(
-    private var images: List<GifImage>
+    private var images: List<GifImage>,
+    private var displayMetrics: DisplayMetrics,
+    private var spanCount: Int
 ) : RecyclerView.Adapter<SearchImageAdapter.ViewHolder>() {
-
     fun setImages(images: List<GifImage>) {
         this.images = images
         notifyDataSetChanged()
@@ -29,8 +33,31 @@ class SearchImageAdapter(
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val imageView = itemView.findViewById<ImageView>(R.id.image_view)
+        private val placeHolders = listOf(
+            ColorDrawable(Color.parseColor("#FF5159")),
+            ColorDrawable(Color.parseColor("#9F4EFF")),
+            ColorDrawable(Color.parseColor("#5BEFFF")),
+            ColorDrawable(Color.parseColor("#42FFAA")),
+            ColorDrawable(Color.parseColor("#FFE96D"))
+        )
+
         fun bind(image: GifImage) {
-            Glide.with(itemView.context).load(image.images.fixedWidth.url).into(imageView)
+            val imageWidth = displayMetrics.widthPixels / spanCount
+            val imageHeight = image.images.fixedWidth.let {
+                (imageWidth  * it.height.toInt()) / it.width.toInt()
+            }
+
+            imageView.apply {
+                layoutParams.width = imageWidth
+                layoutParams.height = imageHeight
+            }
+
+            image.images.fixedWidth.let {
+                Glide.with(itemView.context)
+                    .load(it.url)
+                    .placeholder(placeHolders[layoutPosition % 5])
+                    .into(imageView)
+            }
         }
     }
 }
